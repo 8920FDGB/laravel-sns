1a7e61cf1e04c7ef9f5c1b2eae3dce1a6c1f08b4
8920FDGB <register@old-fshn.com>
2021-09-27 11:53:58 +0900

    vueコンポーネントの作成とビューへの表示

--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -5,6 +5,11 @@
   <input type="text" class="form-control" name="title" required value="{{ $article->title ?? old('title') }}">
 </div>
 
+<div class="form-group">
+  <article-tags-input>
+  </article-tags-input>
+</div>
+
 <div class="md-form">
   <label for=""></label>
   <textarea name="body" id="body" rows="16" class="form-control" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
