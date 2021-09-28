efbcfaee770357d04e62bd7630540eb4ed0a8d06
8920FDGB <register@old-fshn.com>
2021-09-28 11:41:52 +0900

    タグをハッシュタグ風に修正

--- laravel/app/Tag.php
+++ laravel/app/Tag.php
@@ -9,4 +9,10 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    // タグ名に「#」をつけるアクセサ
+    public function getHashtagAttribute(): string
+    {
+        return '#' . $this->name;
+    }
 }
